0763e983f6ca78bf5c730a9f9f74762ffe67226d
scrappyzhang <a7478317@163.com>
2018-02-04 22:24:41 +0800

    增加堆栈的几个实例：运算公式由中缀转换为后缀,并计算

---
+++ 3基本数据结构/栈/active_code_2_39.py
@@ -0,0 +1,126 @@
+"""运算公式由中缀转换为后缀,并计算"""
+'''
+@Time    : 2018/2/4 下午10:16
+@Author  : scrappy_zhang
+@File    : active_code_2_39.py
+'''
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+def infixToPostfix(infixexpr):
+    """
+    :param infixexpr:要转换的公式字符串
+    :return: 返回后缀字符串
+    """
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    opStack = Stack()  # 运算符堆栈
+    postfixList = []  # 后缀表达式列表
+    tokenList = kill_space_split(infixexpr)
+
+    for token in tokenList:
+        if token.isalpha() or token.isdigit():  # 假设只有纯数字和纯字母两种数
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)  # 将左括号压入堆栈
+        elif token == ')':
+            topToken = opStack.pop()  # 弹出堆栈中刚进去的运算符
+            while topToken != '(':  # 如果它不是左括号，则将其添加到postfixList列表，直到遇到左括号
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[token]):  # 运算符堆栈顶端项与当前操作符比较优先级
+                postfixList.append(opStack.pop())  # 当当前操作符优先级小时，把优先级大的操作符添加到postfixList列表
+            opStack.push(token)  # 将运算符压入堆栈
+
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+    print("后缀公式："," ".join(postfixList))
+    return " ".join(postfixList)
+
+
+def kill_space_split(sorce_fix):
+    """
+    拆分为标记列表
+    :param sorce_fix: 原始输入公式
+    :return: 返回标记列表
+    """
+    express1 = []
+    di = ''
+    ai = ''
+    temp = ''
+    for i in sorce_fix:
+        if i != ' ':
+            temp += i
+    print('初始公式：', temp)
+    for i in temp:
+        if i.isalpha():
+            ai += i
+        if i.isdigit():
+            di += i
+            if temp.index(i) == len(temp) - 1:
+                express1.append(di)
+        if i in '+-*/()':
+            if di:
+                # print(di)
+                express1.append(di)
+                di = ''
+            if ai:
+                express1.append(ai)
+                ai = ''
+            express1.append(i)
+    # print(express1)
+    return express1
+
+
+def postfixEval(postfixExpr):
+    """计算后缀公式的值"""
+    operandStack = Stack()
+    tokenList = infixToPostfix(postfixExpr).split()
+
+    for token in tokenList:
+        if token.isdigit():
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token, operand1, operand2)
+            operandStack.push(result)
+    return operandStack.pop()
+
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+if __name__ == '__main__':
+    print("运算结果为：", postfixEval('(72+8) /(3+2) '))
